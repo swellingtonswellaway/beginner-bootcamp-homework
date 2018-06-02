50cb0bd81732d66c77a985ed9e44d984c090a39c
swellingtonswellaway <alice@ALICEs-MBP.lan>
2018-06-02 16:43:43 +0200

    section 1, make function layouts for all the functions on section 2 and added the pictures for sectionn 3

---
+++ your_javascript_here.js
@@ -0,0 +1,42 @@
+// Variables
+// I declared a variable and assigned it to an object with some keys and values.
+// This took me 23 minutes!
+const hero = {
+
+  name: "Codester Fantabulous",
+  heroic: true,
+  inventory: ['spoon', 'candlestick','guitar string','frying pan','banana skin'],
+  health: 10,
+  weapon: {
+      type: "spoon",
+      damage: 4,
+    }
+
+};
+
+
+// Game logic
+const rest = (creature) => {
+  
+
+};
+console.log(rest);
+
+
+const pickUpItem = () => {
+
+};
+
+const dealDamage = () => {
+
+};
+
+const equipWeapon = () => {
+
+};
+
+const doBattle = () => {
+
+};
+
+// UI
