9e91164f37d740533d9ad2f844f934a94dcbd37b
swellingtonswellaway <alice@ALICEs-MBP.lan>
2018-06-03 09:09:15 +0200

    functions, nearly finished

--- your_javascript_here.js
+++ your_javascript_here.js
@@ -1,41 +1,53 @@
 // Variables
 // I declared a variable and assigned it to an object with some keys and values.
-// This took me 23 minutes!
+
 const hero = {
 
-  name: "Codester Fantabulous",
+  name: "Captain Slapstick",
   heroic: true,
-  inventory: ['spoon', 'candlestick','guitar string','frying pan','banana skin'],
-  health: 10,
+  inventory: ['custard pie','anvil', 'big boot','boxing glove','banana peel'],
+  health: 1,
   weapon: {
-      type: "spoon",
-      damage: 4,
+      type: "banana peel",
+      damage: 900
     }
 
 };
-
-
 // Game logic
-const rest = (creature) => {
-  
+const rest = function(creature){
+  if (hero.health < 10){
+return creature.health =10;
+}};
+//maybe without the if statement?
 
-};
-console.log(rest);
 
-
-const pickUpItem = () => {
-
-};
-
-const dealDamage = () => {
-
+const pickUpItem = function(creature, item){
+  creature.inventory.push('custard pie');
+  return creature;
 };
 
-const equipWeapon = () => {
 
+
+const dealDamage = function(attacker, defender){
+defender.health = defender.health - attacker.weapon.damage;
+return defender;
 };
+console.log(defender);
 
-const doBattle = () => {
+
+const equipWeapon = function(creature, index){
+  creature.weapon = creature.inventory[index]
+  return creature.splice();
+  };
+  console.log(creature)
+
+const doBattle = (heroicCreature, creature) => {
+if( heroicCreature === !heroic ){
+  return null;
+}
+
+  If `heroicCreature` is not `heroic` return `null` from this function.
+
 
 };
 
